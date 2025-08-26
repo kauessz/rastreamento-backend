777e46bc80f0fe4d5c7fe0799e4d50e7e20f5640
kauessz <ssz.kaue@gmail.com>
2025-08-25 21:33:30 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/(.*)', cors(corsOptions)); // casa qualquer caminho
 
 // ===== Segurança / Perf (opcionais) =====
 if (helmet) {
